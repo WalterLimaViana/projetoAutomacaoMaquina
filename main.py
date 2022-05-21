6752a51707d2a67ec8d8ef2e945c1015757e0e90
WalterLimaViana <walter.lima.viana@gmail.com>
2022-05-20 22:36:26 -0300

    using continue to change the finish production

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
-def maquina(num_pecas, peso_pecas):
+sair = False
+def liberarMaquina(num_pecas, peso_pecas):
     peso_total = num_pecas * peso_pecas
     if num_pecas <= 6 and peso_total <= 60:
-        print("Máquina liberada")
+        msg = "Máquina liberada!"
     else:
-        print("Máquina bloqueada")
+        msg ="Máquina bloqueada!"
+    return msg
+while True:
 
-num_pecas = int(input("Digite o número de peças: "))
-peso_pecas = float(input("Digite o peso de cada peça: "))
-maquina(num_pecas, peso_pecas)
+    conf = False
+
+    num_pecas = int(input("Digite o número de peças: "))
+    peso_pecas = float(input("Digite o peso de cada peça: "))
+    msg = liberarMaquina(num_pecas, peso_pecas)
+
+    print(msg)
+
+    while conf == False:
+
+        cont = input("Deseja continuar com a produção (s/n): ").lower()
+
+        if cont == 's':
+            conf = True
+            continue
+        elif cont == 'n':
+            conf = True
+            sair = True
+        else:
+            print("Opção inválida")
+
+    if sair == True:
+        break
+    else:
+        continue
+
+print("Produção encerrada")
+
